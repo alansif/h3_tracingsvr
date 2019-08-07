463280a64483741c561c62bd1deba4edef54fbea
Alan Wang <alansif@sina.com>
2019-08-07 23:14:39 +0800

    go on

--- index.js
+++ index.js
@@ -97,28 +97,6 @@ app.get('/api/v1/endoscopes', function(req, res) {
 
 const tianzhu = require('./tianzhu');
 
-app.get('/api/v1/tianzhu', function(req, res) {
-	let edsn = req.query['endoscope'];
-	if (!edsn) {
-		res.status(400).end();
-		return;
-	}
-    let f = async() => {
-        try{
-            let mreq = {endoscope:edsn,fromdate:'2019-08-01',todate:'2019-08-01'};
-            let mres = await query_mdvt(mreq);
-            console.log(mres);
-            let result = await tianzhu.getall(mreq);
-            console.log(result.recordset);
-            res.status(200).json(result.recordset);
-        } catch(err) {
-            console.error(err);
-            res.status(500).json(err);
-        }
-    };
-    f();
-});
-
 app.get('/api/v1/timeline', function(req, res) {
 	let edsn = req.query['endoscope'];
 	let fromdate = req.query['fromdate'] || '';
@@ -127,12 +105,12 @@ app.get('/api/v1/timeline', function(req, res) {
 		res.status(400).end();
 		return;
 	}
-	let mreq = {endoscope:edsn,fromdate:'2019-08-01',todate:'2019-08-01'};
+	let mreq = {endoscope:edsn,fromdate:fromdate,todate:todate};
     let f = async() => {
         try{
 			let mdr = await query_mdvt(mreq);
 			let ml = mdr.map(x => ({
-				title: '机洗', datetime: new Date(x.CycleCompletionDate + 'T' + x.TimeBegin + 'Z'), contents:[
+				stage:2, title: '机洗', datetime: new Date(x.CycleCompletionDate + 'T' + x.TimeBegin + 'Z'), contents:[
 					'机洗结束: ' + x.TimeEnd,
 					'设备ID:　' + x.MachineSerialNumber
 				],
@@ -142,27 +120,26 @@ app.get('/api/v1/timeline', function(req, res) {
 			let tl = tzr.flatMap(x => 
 				x.MarrorStatus === true ?
 				[
-					{title:'测漏初洗', datetime:new Date(x.CleanStart - 1000 * 60), contents:[
+					{stage:1, title:'手洗', datetime:new Date(x.CleanStart - 1000 * 60), contents:[
 						'操作人: ' + x.CardName
 					]},
-					{title:'诊疗使用', datetime:x.UseTime, contents:[
+					{stage:3, title:'诊疗使用', datetime:x.UseTime, contents:[
 						'医生: ' + x.ExamDoctor + '　　　' + '诊疗室: ' + x.ExamRoom,
 						'患者: ' + x.PatientID + '　' + x.PatientName + '　' + x.Sex + '　' + x.Age + '岁'
 					]},
-					{title:'预处理', datetime:x.MarrorCleanTime, contents:[
+					{stage:4, title:'预处理', datetime:x.MarrorCleanTime, contents:[
 						'操作人: ' + x.MarrorCleanPerson,
 						'结束时间: ' + x.MarrorCleanStopTime.toISOString().substring(11, 19)
 					]}
 				] :
 				[
-					{title:'测漏初洗', datetime:new Date(x.CleanStart - 1000 * 60), contents:[
+					{stage:1, title:'手洗', datetime:new Date(x.CleanStart - 1000 * 60), contents:[
 						'操作人: ' + x.CardName
 					]}
 				]
 			);
 			let rl = ml.concat(tl);
 			rl.sort((a, b) => a.datetime - b.datetime);
-			console.log(rl);
             res.status(200).json(rl);
         } catch(err) {
             console.error(err);
